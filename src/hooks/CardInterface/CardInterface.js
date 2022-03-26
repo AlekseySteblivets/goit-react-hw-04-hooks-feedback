5e61a31e8ca05bbee96bd49d21d000654cc51599
AlekseySteblivets <aleksey.steblivets@gmail.com>
2022-03-26 15:10:49 +0200

    билдит

--- src/hooks/CardInterface/CardInterface.js
+++ src/hooks/CardInterface/CardInterface.js
@@ -1,4 +1,4 @@
-import s from './Section.module.css';
+import s from './CardInterface.module.css';
 
 export default function Section({ children }) {
   return <div className={s.card}>{children}</div>;
